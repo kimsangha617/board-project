2e4970eb83eb24298e127c4e2f9b853620504838
haas <kimsangha617@gmail.com>
2024-02-11 17:37:32 +0900

    #1 - data rest test
    data rest test 는 integration으로 접근할 수 밖에 없다
    DB 접근이 일어나므로 안전을 위해 `@Transactional` 추가
    test 안에서는 rollback 정책으로동작한다

---
+++ board-project/src/test/java/com/example/boardproject/controller/DataRestTest.java
@@ -0,0 +1,88 @@
+package com.example.boardproject.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Data REST - api Test")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class DataRestTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+//    public DataRestTest(@Autowired MockMvc mockMvc) {
+//        this.mockMvc = mockMvc;
+//    }
+
+    @DisplayName("[api] articles get list")
+    @Test
+    void givenNothing_whenRequestingArticles_thenReturnsArticlesJsonResponse() throws Exception {
+        //given
+
+        //when & then
+        mockMvc.perform(get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+        ;
+    }
+
+    @DisplayName("[api] article get")
+    @Test
+    void givenNothing_whenRequestingArticle_thenReturnsArticleJsonResponse() throws Exception {
+        //given
+
+        //when & then
+        mockMvc.perform(get("/api/article/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+        ;
+    }
+
+    @DisplayName("[api] articlecomments from article get")
+    @Test
+    void givenNothing_whenRequestingArticleCommentFromArticle_thenReturnsArticleCommentsJsonResponse() throws Exception {
+        //given
+
+        //when & then
+        mockMvc.perform(get("/api/article/1/articleComments"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+        ;
+    }
+
+    @DisplayName("[api] articlecomments get")
+    @Test
+    void givenNothing_whenRequestingArticleComments_thenReturnsArticleCommentsJsonResponse() throws Exception {
+        //given
+
+        //when & then
+        mockMvc.perform(get("/api/articleComments"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+        ;
+    }
+
+    @DisplayName("[api] only articlecomment get")
+    @Test
+    void givenNothing_whenRequestingArticleComment_thenReturnsArticleCommentJsonResponse() throws Exception {
+        //given
+
+        //when & then
+        mockMvc.perform(get("/api/articleComments/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+        ;
+    }
+}
